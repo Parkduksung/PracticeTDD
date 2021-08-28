81742d5ac824a8104999575400c0fd797c6cac54
Parkduksung <duksung1234@naver.com>
2021-08-28 17:22:54 +0900

    [공공API-GoCamping][feature] API 에 대한 설명 주석.

--- publicapi/src/test/java/com/example/publicapi/GoCampingTest.kt
+++ publicapi/src/test/java/com/example/publicapi/GoCampingTest.kt
@@ -6,5 +6,13 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class GoCampingTest {
 
+    /**
+     * 한국관광공사 고캠핑 Api Operation Type
+     * baseList - 기본 정보 목록 조회
+     * locationBasedList - 위치기반 정보 목록 조회
+     * searchList - 키워드 검색 목록 조회
+     * imageList - 이미지정보 목록 조회
+     */
+
 
 }
