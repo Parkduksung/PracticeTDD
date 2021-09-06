334ab09c7a5682a1b0001a8f3f1d030c829ac41c
Parkduksung <duksung1234@naver.com>
2021-09-06 20:12:52 +0900

    [공공API-GoCamping][feature] red, getBaseList 생성.

--- publicapi/src/test/java/com/example/publicapi/GoCampingTest.kt
+++ publicapi/src/test/java/com/example/publicapi/GoCampingTest.kt
@@ -1,13 +1,23 @@
 package com.example.publicapi
 
+import okhttp3.Request
+import okio.Timeout
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.converter.gson.GsonConverterFactory
 
 @RunWith(MockitoJUnitRunner::class)
 class GoCampingTest {
 
+
+    @Mock
+    lateinit var publicApi: PublicApi
 
     /**
      * 한국관광공사 고캠핑 Api Operation Type
@@ -21,6 +31,46 @@ class GoCampingTest {
     fun checkBaseListSuccessTest() {
         val goCampingApi = Retrofit.create<PublicApi>(GOCAPMING_BASE_URL)
 
+    }
+
+
+    private fun mockGetBaseListPublicApi() {
+
+        Mockito.`when`(publicApi.getBaseList()).thenReturn(
+            object : Call<GoCampingResponse> {
+                override fun execute(): Response<GoCampingResponse> {
+                    TODO("Not yet implemented")
+                }
+
+                override fun enqueue(callback: Callback<GoCampingResponse>) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun clone(): Call<GoCampingResponse> {
+                    TODO("Not yet implemented")
+                }
+
+                override fun isExecuted(): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+                override fun cancel() {
+                    TODO("Not yet implemented")
+                }
+
+                override fun isCanceled(): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+                override fun request(): Request {
+                    TODO("Not yet implemented")
+                }
+
+                override fun timeout(): Timeout {
+                    TODO("Not yet implemented")
+                }
+            }
+        )
     }
 
     companion object {
