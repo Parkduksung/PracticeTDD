e15fe1ad983e89018e9ff8f6590040bb3d1e52dd
Parkduksung <duksung1234@naver.com>
2021-09-22 17:26:22 +0900

    [feature] kakao map 띄우기 성공.

--- kakaoapi/src/main/java/com/example/kakaoapi/MainActivity.kt
+++ kakaoapi/src/main/java/com/example/kakaoapi/MainActivity.kt
@@ -1,14 +1,68 @@
 package com.example.kakaoapi
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.example.kakaoapi.databinding.ActivityMainBinding
+import net.daum.mf.map.api.MapView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        initUi()
 
     }
 
+    private fun initUi() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+
+        checkPermission()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            999 -> {
+                loadMapView()
+            }
+
+            else -> {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+        }
+
+    }
+
+    private fun loadMapView() {
+        mapView = MapView(this)
+        binding.containerMap.addView(mapView)
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 999)
+        } else {
+            loadMapView()
+        }
+
+    }
 }
