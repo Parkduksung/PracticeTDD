5eb488f8b80814442a09a6be8af9aac5640e505c
Parkduksung <duksung1234@naver.com>
2021-10-22 23:10:04 +0900

    [feature]PublicTransportationRouteTest init

---
+++ publicapi/src/test/java/com/example/publicapi/PublicTransportationRouteTest.kt
@@ -0,0 +1,8 @@
+package com.example.publicapi
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class PublicTransportationRouteTest {
+}
